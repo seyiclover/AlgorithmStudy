6db2d5603e71b0b2d467cc15b12eb4e8349cdab7
SEYI LEE <leeseyi31@gmail.com>
2024-05-18 10:50:04 +0900

    Update 큰 수 만들기.py

--- Seyi/Greedy/Programmers/큰 수 만들기.py
+++ Seyi/Greedy/Programmers/큰 수 만들기.py
@@ -1,3 +1,4 @@
+# 풀이 1 
 def solution(number, k):
     answer = []
 
@@ -15,3 +16,16 @@ def solution(number, k):
 
     answer = answer[:-k] if k > 0 else answer
     return ''.join(answer)
+
+# 풀이 2 
+def solution(number, k):
+    stack = []
+    for x in number:
+        while stack and k>0 and stack[-1]<x:
+            stack.pop()
+            k-=1
+        stack.append(x)
+    if k != 0:      # k개를 다 제거하지 못했는데 반복문이 종료된 경우
+        stack=stack[:-k]   
+ 
+    return ''.join(stack)
