61e9e29376e8f2db4ecca1246ff25b671ce50675
SEYI LEE <leeseyi31@gmail.com>
2024-08-07 22:44:05 +0900

    Update 네트워크선자르기.py

--- Seyi/DynamicProgramming/네트워크선자르기.py
+++ Seyi/DynamicProgramming/네트워크선자르기.py
@@ -14,3 +14,15 @@ if __name__=="__main__":
     n=int(input())
     d = [0] * (n+1)
     print(cutLine(n))
+
+# Bottom-up 
+n = int(input())
+d = [0] * (n+1)
+d[1] = 1
+d[2] = 2
+
+for i in range(3, n+1):
+    if d[i] ==0:
+        d[i] = d[i-1] + d[i-2]
+
+print(d[n])
