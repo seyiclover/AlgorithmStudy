3cc9afdf2647520324028e0fb773ddd3b1e2b6e7
SEYI LEE <leeseyi31@gmail.com>
2024-08-15 23:01:20 +0900

    Create 가방문제.py

---
+++ Seyi/DynamicProgramming/가방문제.py
@@ -0,0 +1,9 @@
+n, m=map(int, input().split())
+dy=[0]*(m+1)        # 가방의 무게 한도가 j일 때 보석의 최대 가치 저장 
+
+for i in range(n):
+    w, v=map(int, input().split())      # w: 보석의 무게, v: 보석의 가치 
+    for j in range(w, m+1):
+        dy[j]=max(dy[j], dy[j-w]+v)
+        
+print(dy[m])
