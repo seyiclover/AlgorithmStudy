9bf8f21af687329c561e13d463ab54cbff51bd3c
SEYI LEE <leeseyi31@gmail.com>
2024-08-17 00:43:00 +0900

    Update 동전교환.py

--- Seyi/DynamicProgramming/동전교환.py
+++ Seyi/DynamicProgramming/동전교환.py
@@ -3,7 +3,7 @@ coins = list(map(int, input().split()))         # 동전의 종류
 m = int(input())        # 거스름돈 금액
 
 
-dy=[float('inf')] * (m + 1)        # 해당 거스름돈에서 필요한 동전의 최소 개수 저장    
+dy=[float('inf')] * (m + 1)        # 해당 금액을 거슬러주는데 사용되는 동전의 최소 개수 저장    
 dy[0] = 0
 
 for coin in coins:
